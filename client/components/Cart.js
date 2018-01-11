0dd0ab9b4792a09d0ecfc6d8a52a43f7e3827f5f
Alexa Billings <alexa.billings@gmail.com>
2018-01-11 05:41:29 -0600

    cart reducer & mild styling

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,15 +1,78 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { render } from 'react-dom'
+import { deleteProduct } from '../store/cart'
+import { Image, Item } from 'semantic-ui-react'
 // import { Link } from 'react-router-dom'
 
 export class Cart extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
+    const { carts } = this.props
+    console.log('CART: ', carts)
     return (
-      <h1>I am the cart.</h1>
+      <div class="ui divided items">
+      <div class="item">
+        <div class="image">
+          <img src="https://res.cloudinary.com/teepublic/image/private/s--O5uFXkWG--/t_Resized%20Artwork/c_crop,x_10,y_10/c_fit,w_470/c_crop,g_north_west,h_626,w_470,x_0,y_0/g_north_west,u_upload:v1462829015:production:blanks:mtl53ofohwq5goqjo9ke,x_-395,y_-325/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1513727293/production/designs/2198697_1.jpg"></img>
+        </div>
+        <div class="content">
+      <a class="header">Product</a>
+      <div class="meta">
+        <span>Description</span>
+      </div>
+      <div class="description">
+        <p>This is an example product description.</p>
+      </div>
+      <button class="ui icon button"><i class="trash outline icon"></i></button>
+    </div>
+      </div>
+      <div class="item">
+        <div class="image">
+          <img src="https://res.cloudinary.com/teepublic/image/private/s--O5uFXkWG--/t_Resized%20Artwork/c_crop,x_10,y_10/c_fit,w_470/c_crop,g_north_west,h_626,w_470,x_0,y_0/g_north_west,u_upload:v1462829015:production:blanks:mtl53ofohwq5goqjo9ke,x_-395,y_-325/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1513727293/production/designs/2198697_1.jpg"></img>
+        </div>
+        <div class="content">
+      <a class="header">Product</a>
+      <div class="meta">
+        <span>Description</span>
+      </div>
+      <div class="description">
+        <p>This is an example product description.</p>
+      </div>
+      <button class="ui icon button"><i class="trash outline icon"></i></button>
+    </div>
+      </div>
+      <div class="item">
+        <div class="image">
+          <img src="https://res.cloudinary.com/teepublic/image/private/s--O5uFXkWG--/t_Resized%20Artwork/c_crop,x_10,y_10/c_fit,w_470/c_crop,g_north_west,h_626,w_470,x_0,y_0/g_north_west,u_upload:v1462829015:production:blanks:mtl53ofohwq5goqjo9ke,x_-395,y_-325/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1513727293/production/designs/2198697_1.jpg"></img>
+        </div>
+        <div class="content">
+      <a class="header">Product</a>
+      <div class="meta">
+        <span>Description</span>
+      </div>
+      <div class="description">
+        <p>This is an example product description.</p>
+      </div>
+      <button class="ui icon button"><i class="trash outline icon"></i></button>
+    </div>
+      </div>
+      </div>
     )
   }
 }
+
+//container
+const mapStateToProps = carts => {
+  return carts
+}
+
+const mapDispatch = {}
+
+export default connect(mapStateToProps, mapDispatch)(Cart)
 
 //Actions for cart
 //Add product
---
+++ client/store/cart.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+
+// actions
+const INIT_CART = 'INIT_CART'
+const REMOVE_PRODUCT = 'REMOVE_PRODUCT'
+const UPDATE_QUANTITY = 'UPDATE_QUANTITY'
+
+//action creators
+const initCart = carts => ({type: INIT_CART, carts})
+const removeProduct = product => ({type: REMOVE_PRODUCT, product})
+const updateQuantity = product => ({type: UPDATE_QUANTITY, product})
+
+//reducer
+export default function reducer(carts = [], action) {
+  switch (action.type) {
+    case INIT_CART:
+      return action.carts
+
+    case REMOVE_PRODUCT:
+      return carts.filter(product => product.id !== action.product.id)
+
+    case UPDATE_QUANTITY:
+      return carts.map(product => (
+        product.id === action.product.id ? action.product : product
+      ))
+
+    default:
+      return carts
+  }
+}
+
+//thunks
+export const fetchCarts = () => dispatch => {
+  axios.get('/api/carts')
+    .then(res => dispatch(initCart(res.data)))
+    .catch(err => console.error('Error fetching cart: ', err))
+}
+
+export const deleteCart = (cart) => dispatch => {
+  dispatch(removeCart(cart.id))
+  axios.delete(`/api/carts/${cart.id}`)
+    .catch(err => console.error(`Error deleting cart: ${cart}`, err))
+}
