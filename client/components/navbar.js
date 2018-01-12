9f09b54adf52a29d887ce74a74c48102114a5a1f
Patrick Noonan <pnoonan91@gmail.com>
2018-01-12 09:16:02 -0600

    cleanup

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -31,9 +31,6 @@ class Navbar extends Component {
     const {isLoggedIn, handleClick, category, products} = this.props
     return (
       <div id="navbar" className="ui menu">
-
-        <img id="logo" src="/images/logo.png" />
-
         <Link className="item" to="/">
           <img id="logo" src="/images/logo.png" />
         </Link>
