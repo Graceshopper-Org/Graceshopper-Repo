6df179b43f3749135d1a3142abc9ed547249bcb9
Alexa Billings <alexa.billings@gmail.com>
2018-01-15 10:43:15 -0600

    active cart can persist

--- client/routes.js
+++ client/routes.js
@@ -3,8 +3,8 @@ import { connect } from 'react-redux'
 import {Route, Switch, Router} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import history from './history'
-import { setCart, fetchInitialCart } from './store/cart'
-import {Main, Login, Signup, UserHome, UserOrders, SingleOrder, UserAccount, Cart, AllOrders, SingleAdminOrder} from './components'
+import {Main, Login, Signup, UserHome, UserOrders, SingleOrder, UserAccount, Cart, AllOrders, SingleAdminOrder, Admin, AllUsers} from './components'
+import { fetchInitialCart, setCart } from './store/cart'
 import {fetchCategories} from './store/category'
 import AllProducts from './components/Products/AllProducts'
 import ProductDetail from './components/Products/ProductDetail'
@@ -15,33 +15,36 @@ import SearchBar from './components/search'
 import Reviews from './components/reviews'
 import { fetchReviews } from './store/reviews'
 
-
 /**
  * COMPONENT
  */
 
 class Routes extends Component {
   componentDidMount () {
+    const {loadInitialData} = this.props
     const categoryThunk = fetchCategories()
     const productsThunk = fetchProducts();
-
-    let cookie = Number(document.cookie.slice(document.cookie.lastIndexOf('=') + 1))
-
-    this.props.loadInitialData(cookie)
+    loadInitialData()
   }
 
   render () {
-    const {isLoggedIn} = this.props
+    const {isLoggedIn, userId, setActiveCart, setDefaultCart} = this.props
+
+    console.log('isLoggedIn in render', isLoggedIn)
+    let cookie = Number(document.cookie.slice(document.cookie.lastIndexOf('=') + 1))
+
+    if (isLoggedIn){
+      setActiveCart(userId)
+    } else {
+      setDefaultCart(cookie)
+    }
 
     return (
       <Router history={history}>
         <Main>
           <Switch>
             {/* Routes placed here are available to all visitors */}
-
-
             <Route exact path="/cart" component={Cart} />
-
             <Route
               exact
               path="/"
@@ -64,10 +67,6 @@ class Routes extends Component {
               path="/signup"
               component={Signup}
               />
-            <Route
-              path="/testreview/:id"
-              component={Reviews}
-            />
             {
               isLoggedIn &&
                 <Switch>
@@ -91,12 +90,20 @@ class Routes extends Component {
                    component={UserAccount}
                   />
                   <Route
+                   exact path="/admin"
+                   component={Admin}
+                  />
+                  <Route
                    exact path="/admin/orders"
                    component={AllOrders}
                   />
                   <Route
                    exact path="/admin/orders/:orderId"
                    component={SingleAdminOrder}
+                  />
+                  <Route
+                   exact path="/users"
+                   component={AllUsers}
                   />
                 </Switch>
             }
@@ -126,13 +133,16 @@ const mapState = (state) => {
 const mapDispatch = (dispatch) => {
 
   return {
-
-    loadInitialData (cookie) {
+    loadInitialData () {
       dispatch(me())
       dispatch(fetchCategories())
       dispatch(fetchProducts())
       dispatch(fetchReviews())
-      console.log('COOKIE: ', cookie)
+    },
+    setActiveCart (userId) {
+      dispatch(setCart(userId))
+    },
+    setDefaultCart (cookie) {
       dispatch(fetchInitialCart(cookie))
     }
   }
