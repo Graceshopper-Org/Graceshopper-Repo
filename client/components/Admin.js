2db66ba4907d1505838527578ec6ee079d4e159c
Niharika Narain <niharikanarain@gmail.com>
2018-01-16 17:22:57 -0600

    fixes

--- client/components/Admin.js
+++ client/components/Admin.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 function AdminDashboard (){
   return (
-    <div>
-      <h1>Admin Dashboard</h1>
+    <div className="marginClass">
+      <h1>Admin Page</h1>
       <button className="ui massive button"><Link to="/admin/orders">Manage Orders</Link></button>
       <button className="ui massive button"><Link to="/">Manage Products</Link></button>
       <button className="ui massive button"><Link to="/users">Manage Users</Link></button>
